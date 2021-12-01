31df8921f40328386cbe0a111268cf46dcbb94b7
Vitor R <vitorreisdoerfer@hotmail.com>
2021-12-01 17:48:41 -0300

    two new sections added

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,2 +1,2 @@
-console.log("Olár");
+"use strict";var iconsSwiper=new Swiper(".icons-swiper",{slidesPerView:6,spaceBetween:10,slidesPerGroup:3,centeredSlides:!1,loop:!0,navigation:{nextEl:".icons-swiper .swiper-button-next",prevEl:".icons-swiper .swiper-button-prev"},pagination:{el:".swiper-pagination",clickable:!0}});
 //# sourceMappingURL=index.js.map
--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,39 @@
-console.log('Olár')
+"use strict";
+
+var iconsSwiper = new Swiper(".icons-swiper", {
+  slidesPerView: 6,
+  spaceBetween: 10,
+  slidesPerGroup: 3,
+  centeredSlides: false,
+  loop: true,
+  navigation: {
+    nextEl: ".icons-swiper .swiper-button-next",
+    prevEl: ".icons-swiper .swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  // breakpoints: {
+  //   320: {
+  //     slidesPerView: 1,
+  //     spaceBetween: 10,
+  //     slidesPerGroup: 1,
+  //   },
+  //   640: {
+  //     slidesPerView: 3,
+  //     spaceBetween: 10,
+  //     slidesPerGroup: 3,
+  //   },
+  //   768: {
+  //     slidesPerView: 4,
+  //     spaceBetween: 10,
+  //     slidesPerGroup: 4,
+  //   },
+  //   1250: {
+  //     slidesPerView: 6,
+  //     spaceBetween: 10,
+  //     slidesPerGroup: 6,
+  //   },
+  // },
+});
