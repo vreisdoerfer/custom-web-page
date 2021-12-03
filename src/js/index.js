d2b75f4d643c3ac39f2940024f91b7a1cc5fee5f
Vitor R <vitorreisdoerfer@hotmail.com>
2021-12-03 16:18:22 -0300

    mobile version done

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,9 @@
 "use strict";
 
 var iconsSwiper = new Swiper(".icons-swiper", {
-  slidesPerView: 6,
-  spaceBetween: 10,
-  slidesPerGroup: 3,
+  slidesPerView: 1,
+  spaceBetween: 0,
+  slidesPerGroup: 1,
   centeredSlides: false,
   loop: true,
   navigation: {
@@ -14,28 +14,28 @@ var iconsSwiper = new Swiper(".icons-swiper", {
     el: ".swiper-pagination",
     clickable: true,
   },
-  // breakpoints: {
-  //   320: {
-  //     slidesPerView: 1,
-  //     spaceBetween: 10,
-  //     slidesPerGroup: 1,
-  //   },
-  //   640: {
-  //     slidesPerView: 3,
-  //     spaceBetween: 10,
-  //     slidesPerGroup: 3,
-  //   },
-  //   768: {
-  //     slidesPerView: 4,
-  //     spaceBetween: 10,
-  //     slidesPerGroup: 4,
-  //   },
-  //   1250: {
-  //     slidesPerView: 6,
-  //     spaceBetween: 10,
-  //     slidesPerGroup: 6,
-  //   },
-  // },
+  breakpoints: {
+    // 320: {
+    //   slidesPerView: 1,
+    //   spaceBetween: 10,
+    //   slidesPerGroup: 1,
+    // },
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 5,
+      slidesPerGroup: 1,
+    },
+    500: {
+      slidesPerView: 4,
+      spaceBetween: 10,
+      slidesPerGroup: 2,
+    },
+    800: {
+      slidesPerView: 6,
+      spaceBetween: 10,
+      slidesPerGroup: 3,
+    },
+  },
 });
 
 var showcaseSwiper = new Swiper(".showcase-swiper", {
